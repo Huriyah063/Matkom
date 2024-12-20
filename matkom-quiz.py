686629b800cea151f6e8975ba1a1c827e6484881
Huriyah063 <hurihuriyahhuri@gmail.com>
2024-12-20 10:26:22 +0700

    Add files via upload

---
+++ matkom-quiz.py
@@ -0,0 +1,10 @@
+mulai = 100
+akhir = 500
+
+kelipatan_3_dan_5 = [x for x in range(mulai, akhir + 1) if x % 3 == 0 and x % 5 == 0]
+
+jumlah = len(kelipatan_3_dan_5)
+
+print("Bilangan kelipatan 3 dan 5 antara 100 dan 500:")
+print(kelipatan_3_dan_5)
+print("\nJumlah bilangan kelipatan 3 dan 5:", jumlah)
